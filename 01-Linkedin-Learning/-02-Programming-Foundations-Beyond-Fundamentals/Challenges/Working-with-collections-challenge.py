9223c807bd5f52207dcfcbf0cce6119c22bd496a
Yousef Maher El-khayat <106788176+YousefMaher179@users.noreply.github.com>
2023-07-15 04:04:22 +0300

    Update Working-with-collections-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Working-with-collections-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Working-with-collections-challenge.py
@@ -1,1 +1,19 @@
+stars = [
+     'sol',
+     'Alpha Centauri',
+     'Barnard',
+     'Wolf 395',
+]
 
+peaks = {
+    'African' : 'Kilimanjaro',
+    'Antarctic' : 'vinson',
+    'Australian' : 'Puncak jaya',
+    'Eurasian' : 'Everest',
+    'North_American' : 'Denali',
+    'Pacific' : 'Mauna Kea',
+    'South_American' : 'Aconcaugua',
+}
+
+print(stars[3])
+print(peaks['Pacific'])
