08afcc7b0be4ac89864fbbd8c1bf44780a3c54fd
Yousef Maher El-khayat <106788176+YousefMaher179@users.noreply.github.com>
2023-07-17 11:35:26 +0300

    Add files via upload

---
+++ 04-Serverless-Functions-Netlify/Projects/Serverless-Vanilla/assets/data.js
@@ -0,0 +1,36 @@
+const items = [
+  {
+    id: "recmg2a1ctaEJNZhu",
+    name: "utopia sofa",
+    image: {
+      url: "https://i.pinimg.com/564x/ab/84/93/ab849324fa828dbf6f1f7b7d28b7a06e.jpg",
+    },
+    price: 39.95,
+  },
+  {
+    id: "recvKMNR3YFw0bEt3",
+    name: "entertainment center",
+    image: {
+      url: "https://i.pinimg.com/564x/a4/95/39/a4953940968dced1c0765e67a7b86527.jpg",
+    },
+    price: 29.98,
+  },
+  {
+    id: "recxaXFy5IW539sgM",
+    name: "albany sectional",
+    image: {
+      url: "https://i.pinimg.com/564x/29/df/fb/29dffb182f3b23322df51b9a5ba0bc78.jpg",
+    },
+    price: 10.99,
+  },
+  {
+    id: "recyqtRglGNGtO4Q5",
+    name: "leather sofa",
+    image: {
+      url: "https://i.pinimg.com/564x/6a/73/96/6a7396c73efba7ce2f10d538d8d7bf67.jpg",
+    },
+    price: 9.99,
+  },
+];
+
+module.exports = items;
